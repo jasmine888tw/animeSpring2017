57c3b427264635550ccff91ff3d798a012b549dd
“jasmine888tw” <jasmine888tw@gmail.com>
2017-05-09 13:49:53 +0800

    Initial Commit

---
+++ animetSpring2017Tests/animetSpring2017Tests.swift
@@ -0,0 +1,36 @@
+//
+//  animetSpring2017Tests.swift
+//  animetSpring2017Tests
+//
+//  Created by juicemeart on 2017/5/9.
+//  Copyright © 2017年 juicemeart. All rights reserved.
+//
+
+import XCTest
+@testable import animetSpring2017
+
+class animetSpring2017Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
